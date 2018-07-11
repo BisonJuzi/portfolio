f9a5ada1658a42847bc4fc7710d6969f804fd086
LiuJuan <liujuan_chener@foxmail.com>
2018-07-11 16:20:12 +0800

    前端 + 访问静态文件

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,13 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    # 这样后台就会显示title，而不是 Blog Object(1)
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
